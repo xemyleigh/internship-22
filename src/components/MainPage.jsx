2ab150f63662f8a7a0cd1e53abc8f1f3aa79c096
alexander <emyleigh@inbox.ru>
2022-11-14 18:21:16 +0300

    fix & update MainPage

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,10 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card, Button, Badge, Spinner } from 'react-bootstrap'
+import { Container, Card, Button, Badge, Spinner } from 'react-bootstrap'
+import { fetchNews } from "../slices/newsSlice";
+import { Link } from 'react-router-dom';
 
-import { actions as newsActions, fetchNews } from "../slices/newsSlice";
-
-const Story = ({ author, score, comments, time, title, url }) => {
+const Story = (storyData) => {
+    const { id, author, score, comments, time, title, url } = storyData
     const date = new Date(time * 1000)
     const dateOptions = {
         year: 'numeric',
@@ -27,46 +26,30 @@ const Story = ({ author, score, comments, time, title, url }) => {
             </Card.Header>
             <Card.Body>
                 <Card.Title><a href={url}>{title}</a></Card.Title>
-                <Button className='mb-3 mt-2'variant="primary" href={url}>Visit topic</Button>
+                <Link to={{ pathname: `/${id}`, state: storyData }} className='btn btn-primary mb-3 mt-2'>Visit topic</Link>
+                {comments && <p>{`Count: ${comments.length}`}</p>}
                 <p>{formattedDate}</p>
             </Card.Body>
         </Card>
-
     )
 }
 
 const MainPage = () => {
     const dispatch = useDispatch()
-    const news = useSelector(state => state.newsInfo.news)
+    const { entities, ids } = useSelector(state => state.newsInfo.news)
+    const news = ids.map((id) => entities[id])
     const isLoading = useSelector(state => state.newsInfo.isLoading)
-    console.log(isLoading)
-    console.log('dispatching')
-    const [ updateTimer, setUpdateTimer ] = useState(0)
-    const [ isButtonLoading, setButtonLoadingStatus ] = useState(false)
-
-    setTimeout(function run() {
-        setUpdateTimer(updateTimer + 1)
-        setTimeout(run, 60000);
-    }, 60000);    
-
-    useEffect(() => {
-        console.log('324324234234')
-        dispatch(fetchNews())
-        // setFirstDownloadStatus('done')
-
-    }, [updateTimer])
-
     const updateButtonHandler = () => {
         dispatch(fetchNews())
     }
     
 
     return (
-        <>
-            <div className='text-center mb-5'>
+        <Container>
+            <div className='text-center mb-5 mt-5'>
                 <h1 className='mb-4'>Hacker News Interface</h1>
                 <Button variant='primary text-white' className='p-2 px-3' disabled={isLoading} onClick={updateButtonHandler}>
-                    {(isLoading) ? (
+                    {(isLoading) && (
                         <Spinner
                             as="span"
                             animation="border"
@@ -75,15 +58,15 @@ const MainPage = () => {
                             aria-hidden="true"
                             className='me-2'
                         />
-                    ) : null}
+                    )}
                     Refresh news
                     </Button>
             </div>
             
             <ul>
-                {news.map(({ id, by, score, kids, time, title, url }) => <Story key={id} author={by} score={score} comments={kids} time={time} title={title} url={url} />)}
+                {news.map(({ id, by, score, kids, time, title, url }) => <Story key={id} id={id} author={by} score={score} comments={kids} time={time} title={title} url={url} />)}
             </ul>
-        </>
+        </Container>
     )
 }
 

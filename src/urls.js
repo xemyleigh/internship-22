720ebeb9ef95825ad9a11e267d0a8f84b1558916
alexander <emyleigh@inbox.ru>
2022-11-14 18:11:10 +0300

    add link

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,4 @@
 export default {
     getNewStories: () => 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty',
-    getStoryData: (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-
+    getItemData: (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`,
 }

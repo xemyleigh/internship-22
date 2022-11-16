06e5ed5a188fbff5c9dcd26f81f5220ffd90e6d3
alexander <emyleigh@inbox.ru>
2022-11-16 18:59:06 +0300

    remove unnecessary imports and code in app.js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from './components/MainPage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import StoryPage from './components/StoryPage'
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchNews } from "./fetchApi";
-import { ToastContainer as Toaster, toast } from 'react-toastify';
+import { ToastContainer as Toaster } from 'react-toastify';
 
 function App() {
-
-  // const [ updateTimer, setUpdateTimer ] = useState(0)
-  // const dispatch = useDispatch()
-
-  // setTimeout(function run() {
-  //     setUpdateTimer(updateTimer + 1)
-  //     setTimeout(run, 60000);
-  // }, 60000);    
-
-  // useEffect(() => {
-  //   try {
-  //     dispatch(fetchNews())
-  //   } catch(e) {
-  //     if (e.isAxiosError) {
-  //       toast('No internet connection')
-  //     } else {
-  //       toast('Unknown error')
-  //     }
-  //   }
-  // }, [updateTimer])
-
   return (
     <Router>
       <Switch>
